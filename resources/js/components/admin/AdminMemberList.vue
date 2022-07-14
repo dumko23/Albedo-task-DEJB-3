4c9e457de2b26bcf79398e7c18f93806c330df46
Dima Parkhomenko <parhomenko.dmit@gmail.com>
2022-07-14 16:47:08 +0300

    Minor changes

--- resources/js/components/admin/AdminMemberList.vue
+++ resources/js/components/admin/AdminMemberList.vue
@@ -19,14 +19,14 @@
                          @hideModal="toggleEditParent"></edit-member>
 
             <div class="w-100 justify-content-center align-self-center mx-auto mb-5 ">
-                <table class='table member-table align-self-center'>
+                <table class='table member-table align-self-center' ref="table">
                     <thead class="table-secondary">
                     <tr class="trHead">
                         <th class='photoHead text-center'>Photo</th>
-                        <th class='nameHead text-center'>Full Name</th>
-                        <th class='subjectHead text-center'>Report Subject</th>
-                        <th class='emailHead text-center'>Email</th>
-                        <th class='visibilityHead text-center'>Visibility</th>
+                        <th class='text-center'>Full Name</th>
+                        <th class='text-center'>Report Subject</th>
+                        <th class='text-center'>Email</th>
+                        <th class='text-center'>Visibility</th>
                         <th></th>
                     </tr>
                     </thead>
@@ -34,9 +34,9 @@
                     <tr class="tr-50" v-for="member in $data.members">
                         <td class="img-td"><img class=" img-fluid" :src='"../"+member.photo'
                                                 alt='user photo'></td>
-                        <td class="td-content">{{ member.firstName }} {{ member.lastName }}</td>
-                        <td class="td-content">{{ member.subject }}</td>
-                        <td class="td-content">
+                        <td>{{ member.firstName }} {{ member.lastName }}</td>
+                        <td>{{ member.subject }}</td>
+                        <td>
                             <a class="navbar-brand text-dark email-link"
                                :href="`mailto:${member.email}`">{{ member.email }}</a></td>
                         <td class="td-content">{{ member.visibility ? 'visible' : 'hidden' }}</td>
@@ -47,11 +47,11 @@
                                     <font-awesome-icon icon="fa-solid fa-pen"/>
                                 </button>
                                 <button class="icon-button w-100" v-if="!!member.visibility === false"
-                                   @click="toggleShow(member)">
+                                        @click="toggleShow(member)">
                                     <font-awesome-icon icon="fa-solid fa-eye"/>
-                                </button >
+                                </button>
                                 <button class="icon-button w-100" v-if="!!member.visibility === true"
-                                   @click="toggleShow(member)">
+                                        @click="toggleShow(member)">
                                     <font-awesome-icon icon="fa-solid fa-eye-slash"/>
                                 </button>
                                 <button class="icon-button w-100" @click="deleteMemberCall(member)">
@@ -66,7 +66,7 @@
 
             </div>
         </div>
-        <a v-if="this.members.length > 1" style="position: fixed; bottom: 0; left: 0;"
+        <a v-show="this.members.length > 7" style="position: fixed; bottom: 0; left: 0;"
            class="my-link navbar-brand text-dark back-link" href="#">To the top</a>
     </div>
 
@@ -142,7 +142,7 @@ export default {
             this.memberToDelete = member.email;
             this.confirmDelete = true;
         },
-        editMemberCall(member){
+        editMemberCall(member) {
             axios.get(`/getMemberFullData/:${member.memberId}`)
                 .then(res => {
                         this.memberToEdit = res.data[0];
@@ -153,7 +153,6 @@ export default {
         },
         toggleEditParent(data) {
             this.editMember = data
-
         },
     },
     beforeMount() {
@@ -178,25 +177,15 @@ html {
 table {
     width: 100%;
     border-collapse: collapse;
-    /*border: 2px solid rgb(200, 200, 200);*/
     border-bottom: none;
     letter-spacing: 1px;
     font-size: 0.8rem;
-    /*height: 500px;*/
     box-sizing: border-box;
 }
-
-/*td, th {
-    border: 1px solid rgb(190, 190, 190);
-}*/
 
 td {
     text-align: center;
     vertical-align: middle;
-}
-
-.td-content {
-
 }
 
 img {
@@ -247,11 +236,11 @@ a {
     background-color: #ffffff;
 }
 
-.icon-button:hover{
+.icon-button:hover {
     background-color: #dee2e6;
 }
 
-.tr-50{
+.tr-50 {
     height: 50px;
 }
 </style>
