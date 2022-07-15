d1003be1e0a4080b4603b9e23ab6a02e20241f55
Dima Parkhomenko <parhomenko.dmit@gmail.com>
2022-07-15 17:27:38 +0300

    Moving scripts to the footer section

--- resources/views/layouts/admin-bar.blade.php
+++ resources/views/layouts/admin-bar.blade.php
@@ -9,9 +9,6 @@
 
     <title>{{ $title }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -73,5 +70,6 @@
         @yield('content')
     </main>
 </div>
+<script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
